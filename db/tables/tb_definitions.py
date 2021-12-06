e60bcefa5fddd4c2f20c8f7f8e233629a9c981d9
Jefferson <silva.o.jefferson@gmail.com>
2021-12-06 18:47:35 -0300

    Add support for authors and sources name in the database

--- db/tables/tb_definitions.py
+++ db/tables/tb_definitions.py
@@ -17,21 +17,24 @@ class TableArticles(Base):
     __tablename__ = "tb_articles"
     __table_args__ = (UniqueConstraint('hashed_url', name="urlx_1"),)
     id = Column('id', Integer, primary_key=True)
+    source_id = Column('source_id', Integer, ForeignKey('tb_sources.id'), nullable=False)
     hashed_url = Column('hashed_url', BigInteger, nullable=False)
     url = Column('url', Text(2048), nullable=False)
     content = Column('content', Text(30000), nullable=False)
     published_time = Column('published_time', Date)
-    source_id = Column('source_id', Integer, ForeignKey('tb_sources.id'), nullable=False)
     title = Column('title', Text(200))
-    added = Column('added', Date)
     keywords = Column('keywords', String(400))
     section = Column('section', String(50))
     site_name = Column('site_name', String(50))
+    authors = Column('authors', String(100))
+    added = Column('added', Date)
+    sent = Column('sent', Boolean, nullable=False)
     table_sources = relationship('TableSources')
 
 class TableSources(Base):
     __tablename__ = "tb_sources"
     __table_args__ = (UniqueConstraint('home_url', name="hrulx_1"),)
     id = Column('id', Integer, primary_key=True)
+    name = Column('name', String(100))
     home_url = Column('home_url', String(50), nullable=False)
     enabled = Column('enabled', Boolean)
