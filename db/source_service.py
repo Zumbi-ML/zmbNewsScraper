468c997c0c5badd455f7d9ae3f9d6b2605bafd6e
Jefferson <silva.o.jefferson@gmail.com>
2021-12-12 10:07:30 -0300

    Change how sources are added to the DB, by using a map mirroring the sources table

--- db/source_service.py
+++ db/source_service.py
@@ -26,16 +26,30 @@ class SourceService(BaseService):
             home_urls.append({"id": row.id, "home_url": row.home_url})
         return home_urls
 
-    def persist_all(self, home_urls):
+    def persist(self, source_map):
         """
-        Persists all URLs in the database
+        Adds a source_map into the session
         Args:
-            home_urls: a list of news sites home URLs.
-            E.g.: http://folha.uol.com.br
+            source_map: a map mirroring the table sources.
+            E.g.:
+                {'name': Folha de S. Paulo',
+                 'url': 'http://folha.uol.com.br',
+                 'enabled': True,}
         """
-        for url in home_urls:
-            source = TableSources( \
-                home_url=url,
-                enabled=True,
-            )
-            self._session.add(source)
+        source = TableSources( \
+            name=source_map['name'],
+            home_url=source_map['home_url'],
+            enabled=source_map['enabled'],
+        )
+        self._session.add(source)
+
+
+    def persist_all(self, source_map_lst):
+        """
+        Adds a list of source_maps into the session
+        Args:
+            source_map_lst: a list of source_maps.
+                            See the method persist for an example of source_map.
+        """
+        for source_map in source_map_lst:
+            self.persist(source_map)
--- source_manager.py
+++ source_manager.py
@@ -2,12 +2,19 @@
 
 from db.source_service import SourceService
 
-def add_source(sources_lst):
+def add_sources(source_map_lst):
     """
     Adds a list of sources into the database
     """
     with SourceService() as source_svc:
-        source_svc.persist_all(sources_lst)
+        source_svc.persist_all(source_map_lst)
+
+def add_source(source_map):
+    """
+    Add a source map into the database
+    """
+    with SourceService() as source_svc:
+        source_svc.persist(source_map)
 
 def find_all_enabled():
     """
@@ -18,4 +25,6 @@ def find_all_enabled():
         all_sources = source_svc.find_all_enabled()
     return all_sources
 
-#add_source(['https://www.folha.uol.com.br', 'https://www.estadao.com.br/'])
+#add_source({'name': 'Folha de São Paulo', 'home_url': 'https://www.folha.uol.com.br', 'enabled': True})
+#add_source({'name': 'Estadão', 'home_url': 'https://www.estadao.com.br/', 'enabled': True})
+#add_source({'name': 'O Globo', 'home_url': 'https://oglobo.globo.com/', 'enabled': True})
