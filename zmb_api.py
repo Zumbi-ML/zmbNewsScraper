52c7609d5a6c0078803364a0f7bfad8fc652c295
Jefferson <silva.o.jefferson@gmail.com>
2021-12-20 17:12:33 -0300

    Add  ZmbApi object to handle addresses of API

---
+++ zmb_api.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class ZmbApi:
+
+    class Article:
+
+        def path():
+            api_host = os.getenv('API_HOST')
+            api_port = os.getenv('API_PORT')
+            api_version = os.getenv('API_VERSION')
+            return f"{api_host}:{api_port}/{api_version}/articles"
